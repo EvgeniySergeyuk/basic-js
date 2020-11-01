a7f16afb2adb8ea5d77439c574409e6e7c4de6bb
EvgeniySergeyuk <gibrosha09@gmail.com>
2020-11-01 13:11:05 +0300

    feat: implement function vigenere-cipher()

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,46 @@
+const { fake } = require("sinon");
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+      this.type = arguments.length === 0 ? true : type;      
+  }
+  encrypt(str, key) {
+      str = str.toLowerCase();
+      key = key.toLowerCase();
+      let alph = 'abcdefghijklmnopqrstuvwxyz';
+      let rez = '';
+      let j = 0;
+      for (let i = 0; i < str.length; i++ , j++) {
+          if (alph.indexOf(str[i]) === -1) {
+              rez += str[i];
+              j--;
+              continue;
+          }
+          if (j === key.length) j = 0;
+          let sum = alph.indexOf(str[i]) + alph.indexOf(key[j]);
+          rez += alph[sum >= alph.length ? sum - alph.length : sum];
+      }
+      return this.type ? rez.toUpperCase() : rez.toUpperCase().split('').reverse().join('');
+  }
+
+  decrypt(str, key) {
+    str = str.toLowerCase();
+    key = key.toLowerCase();
+    let alph = 'abcdefghijklmnopqrstuvwxyz';
+    let rez = '';
+    let j = 0;
+    for(let i = 0; i < str.length; i++, j++){
+      if(alph.indexOf(str[i]) === -1){ 
+          rez += str[i];
+          j--;
+          continue;
+      }
+      if(j === key.length) j = 0;
+          let sum = alph.indexOf(str[i]) - alph.indexOf(key[j]);
+          rez += alph[sum < 0 ? sum + alph.length : sum];
+    }
+    return this.type ? rez.toUpperCase() : rez.toUpperCase().split('').reverse().join('');
   }
 }
 
